ed52d7453a6b2687ff52c99ea9c58eb33841f058
wnkbll <arzhevitin.g@gmail.com>
2023-05-23 16:54:41 +0500

    Changed styles

--- src/components/Repos.jsx
+++ src/components/Repos.jsx
@@ -5,7 +5,7 @@ function Repos({ repos }) {
 		<div className="flex flex-col items-center gap-3">
 			{repos.map((repo) => {
 				return (
-					<div key={repo.id} className="w-1/2 h-56 text-white bg-github-side border-2 border-solid border-github-border rounded-lg mt-2 p-2 hover:border-gray-500">
+					<div key={repo.id} className="w-1/2 h-56 text-white bg-github-side border-2 border-solid border-github-border rounded-lg mt-2 p-2 hover:border-gray-500 hover:bg-gray-900">
 						{repo.name}
 						<img
 							className="w-16 h-16 rounded-full"
